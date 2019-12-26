4ff632e44e55dc293a2e3a1876a96220695ef2ba
Kajonczyk <kopczackikamil2000@gmail.com>
2019-12-26 19:38:00 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import { GlobalStyle } from "./Utils/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./Utils/theme";
 import NavbarProvider from "./Shared/NavbarContext";
+import {Router,Switch,Route} from "react-router-dom";
+import history from "./Utils/history"
 function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
         <NavbarProvider>
+          <Router history={history}>
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/about" exact component={AboutPage} />
+            </Switch>
           <GlobalStyle />
-          {/* <HomePage /> */}
-          <AboutPage />
+          </Router>
+
+          
         </NavbarProvider>
       </ThemeProvider>
     </div>
