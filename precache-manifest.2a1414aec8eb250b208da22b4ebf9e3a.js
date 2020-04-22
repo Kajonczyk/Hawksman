fa53d42160a86c5cb8539c1e0dc8b0de0038dd88
Kajonczyk <kopczackikamil2000@gmail.com>
2020-04-22 14:40:41 +0200

    Updates

---
+++ precache-manifest.2a1414aec8eb250b208da22b4ebf9e3a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed8da7d71ddd0c7b3336c54788856a2",
+    "url": "/Hawksman/index.html"
+  },
+  {
+    "revision": "da3a46669686c0945b50",
+    "url": "/Hawksman/static/js/2.a0dc8c59.chunk.js"
+  },
+  {
+    "revision": "75031fbd744675419f31",
+    "url": "/Hawksman/static/js/main.784a174f.chunk.js"
+  },
+  {
+    "revision": "9d41a374830fdc7028f5",
+    "url": "/Hawksman/static/js/runtime-main.0af70591.js"
+  },
+  {
+    "revision": "dca2f41ab1e30660a9dcdd92685ccd2c",
+    "url": "/Hawksman/static/media/bgContact.dca2f41a.jpg"
+  },
+  {
+    "revision": "ac377f49d4455ac6259c02cf99c4f52a",
+    "url": "/Hawksman/static/media/bg_hero.ac377f49.jpg"
+  },
+  {
+    "revision": "8b2913be1165ed3faf69211af3ff725b",
+    "url": "/Hawksman/static/media/eagle.8b2913be.png"
+  },
+  {
+    "revision": "28db0503f0a6facc0ada5aca7823ef4a",
+    "url": "/Hawksman/static/media/heroImage.28db0503.png"
+  },
+  {
+    "revision": "2aad6cb4bee5c29f6f140a7ea932600f",
+    "url": "/Hawksman/static/media/house.2aad6cb4.jpg"
+  },
+  {
+    "revision": "112bd72d0b869e006b8d503062f5110b",
+    "url": "/Hawksman/static/media/logo_hawksman.112bd72d.png"
+  },
+  {
+    "revision": "3696776fa88103233890db98d24ed1c6",
+    "url": "/Hawksman/static/media/portraitJames.3696776f.png"
+  },
+  {
+    "revision": "654cd0da0d781244d26332a374816373",
+    "url": "/Hawksman/static/media/property1.654cd0da.jpg"
+  },
+  {
+    "revision": "67d53ddfe4c51ceced17f179e2ca1ef1",
+    "url": "/Hawksman/static/media/property2.67d53ddf.jpg"
+  },
+  {
+    "revision": "75a2bc99207a39127a7ed6053622f3b0",
+    "url": "/Hawksman/static/media/property3.75a2bc99.jpg"
+  },
+  {
+    "revision": "6541ad80bd1f88ec9f15b486c6b3b5e6",
+    "url": "/Hawksman/static/media/property4.6541ad80.jpg"
+  },
+  {
+    "revision": "d79569ed3edccadfdbdca696e0317636",
+    "url": "/Hawksman/static/media/river.d79569ed.jpg"
+  },
+  {
+    "revision": "48021db58cfb4443f57ed91ff625a674",
+    "url": "/Hawksman/static/media/view.48021db5.jpg"
+  }
+]);
